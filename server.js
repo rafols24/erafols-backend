bb31e840ced8c00dbc7b4f0aa64cb548800ceadc
rafols24 <58802200+rafols24@users.noreply.github.com>
2021-04-09 15:42:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const UserRouter = require('./routes/user');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
